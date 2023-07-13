6d34e092df1a346b13211f489ba80009184e31a6
yashsawwashere <yashsawwashere103@gmail.com>
2023-07-13 17:23:41 +0530

    service class customer service created vendor service created vendor menu item service created

---
+++ src/main/java/com/bootcoding/restaurant/exercise/Test.java
@@ -0,0 +1,43 @@
+package com.bootcoding.restaurant.exercise;
+
+import com.bootcoding.restaurant.util.MenuItemEnum;
+/*
+
+public class Test {
+    public static void main(String[] args) {
+
+        MenuItemEnum paneerMasala=MenuItemEnum.PANEER_MASALA;
+        System.out.println(paneerMasala.isVeg());
+        System.out.println(paneerMasala.getCategory());
+        System.out.println(paneerMasala.name());
+
+        MenuItemEnum prawnsMasla=MenuItemEnum.PRAWNS_MASALA;
+        System.out.println(paneerMasala.isVeg());
+        System.out.println(paneerMasala.getCategory());
+        System.out.println(paneerMasala.name());
+
+        MenuItemEnum fishMasala=MenuItemEnum.FISH_MASALA;
+        System.out.println(paneerMasala.isVeg());
+        System.out.println(paneerMasala.getCategory());
+        System.out.println(paneerMasala.name());
+
+        MenuItemEnum prawnsCurry=MenuItemEnum.PRAWNS_CURRY;
+        System.out.println(paneerMasala.isVeg());
+        System.out.println(paneerMasala.getCategory());
+        System.out.println(paneerMasala.name());
+
+        MenuItemEnum =MenuItemEnum.PRAWNS_CURRY;
+        System.out.println(paneerMasala.isVeg());
+        System.out.println(paneerMasala.getCategory());
+        System.out.println(paneerMasala.name());
+
+        MenuItemEnum prawnsCurry=MenuItemEnum.PRAWNS_CURRY;
+        System.out.println(paneerMasala.isVeg());
+        System.out.println(paneerMasala.getCategory());
+        System.out.println(paneerMasala.name());
+
+
+
+    }
+}
+*/
